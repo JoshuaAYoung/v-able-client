165d77024ea26463d751ec571c8ee688ed219422
Josh Young <joshua@young.net>
2020-05-25 19:51:02 -0600

    final loading tweaks

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  // 'https://v-able.herokuapp.com/api' || 'http://localhost:8000/api',
+  'https://v-able.herokuapp.com/api' || 'http://localhost:8000/api',
   TOKEN_KEY: 'v-able-client-auth-token',
 };
