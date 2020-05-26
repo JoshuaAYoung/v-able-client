bca4eb8e30f9c6bf7d89fb41477b28e9877ae5db
Josh Young <joshua@young.net>
2020-05-25 20:25:16 -0600

    fixed server connection

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  'https://v-able.herokuapp.com/api' || 'http://localhost:8000/api',
+  API_ENDPOINT:
+    'https://v-able.herokuapp.com/api' || 'http://localhost:8000/api',
   TOKEN_KEY: 'v-able-client-auth-token',
 };
