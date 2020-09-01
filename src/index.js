6c44f702bced3524c5c348a2229ae3e8ddad596c
Josh Young <joshua@young.net>
2020-08-31 22:42:48 -0600

    react-ga install

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { VableProvider } from './context/VableContext';
+import ReactGA from 'react-ga';
 import App from './App';
 import './index.css';
+
+ReactGA.initialize('UA-173566344-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <BrowserRouter>
